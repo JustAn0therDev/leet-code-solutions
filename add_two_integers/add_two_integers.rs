03ec0c7bf09216e61b92a3def846b980cecbbc16
Ruan Montelo <ruanmontelo@gmail.com>
2022-07-02 00:24:04 -0300

    Commiting 'Add Two Integers'

---
+++ add_two_integers/add_two_integers.rs
@@ -0,0 +1,11 @@
+/*
+	Given two integers num1 and num2, return the sum of the two integers.
+*/
+
+fn add_two_integers(num1: i32, num2: i32) -> i32 {
+	return num1 + num2;
+}
+
+fn main() {
+	println!("{:?}", add_two_integers(1, 2));
+}
